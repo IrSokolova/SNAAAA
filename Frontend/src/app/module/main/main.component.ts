a94cac56bb590f8ba3a2f57dd3838a32b738fd54
ritasidorska <m.sidorskaya@innopolis.university>
2022-12-07 16:03:04 +0300

    Файлики всякие посоздавала, сайт поверстала цуцуть, устала

---
+++ Frontend/src/app/module/main/main.component.ts
@@ -0,0 +1,12 @@
+import { Component, OnDestroy, OnInit } from '@angular/core';
+
+@Component({
+    selector: 'app-new-appointment',
+    templateUrl: './new-appointment.component.html',
+    styleUrls: ['./new-appointment.component.css'],
+    // providers: [MessageService],
+})
+
+export class MainComponent implements OnInit, OnDestroy {
+
+}
