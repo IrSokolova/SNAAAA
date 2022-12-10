8a2fb3d9c84d24887c2a789c9939c5adbe5c8dd3
ritasidorska <m.sidorskaya@innopolis.university>
2022-12-10 16:30:44 +0300

    Устала

---
+++ Frontend/src/app/module/app-routing.module.ts
@@ -0,0 +1,13 @@
+import { NgModule } from '@angular/core';
+import { Routes, RouterModule } from '@angular/router';
+import { TrainListComponent } from './train-list/train-list.component';
+
+const routes: Routes = [
+    { path: 'train', component: TrainListComponent }
+];
+
+@NgModule({
+    imports: [RouterModule.forRoot(routes)],
+    exports: [RouterModule]
+})
+export class AppRoutingModule { }
